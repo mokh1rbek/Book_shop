8fb776729325b9bec133252b70e61f4436f4d753
mokhirbek <mokhirbekabduvaliev@gmail.com>
2022-12-21 17:25:57 -0500

    book shop

---
+++ api/handler/handler.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	"github.com/mokh1rbek/Book_shop/storage"
+)
+
+type HandlerV1 struct {
+	storage storage.StorageI
+}
+
+func NewHandlerV1(storage storage.StorageI) *HandlerV1 {
+	return &HandlerV1{
+		storage: storage,
+	}
+}
---
+++ models/order.go
@@ -0,0 +1,53 @@
+package models
+
+type OrderPrimarKey struct {
+	Id string `json:"order_id"`
+}
+
+type CreateOrderSwagger struct {
+	User_id string `json:"user_id"`
+	Book_id string `json:"book_id"`
+}
+
+type CreateOrder struct {
+	User_id string  `json:"user_id"`
+	Book_id string  `json:"book_id"`
+	Payed   float64 `json:"payed"`
+}
+
+type Order struct {
+	Id        string  `json:"order_id"`
+	User_id   string  `json:"user_id"`
+	Book_id   string  `json:"book_id"`
+	Payed     float64 `json:"payed"`
+	CreatedAt string  `json:"created_at"`
+	UpdatedAt string  `json:"updated_at"`
+}
+
+type OrderGroup struct {
+	FullName   string  `json:"fullname"`
+	TotalPayed float64 `json:"total_payed"`
+}
+
+type UpdateOrderSwagger struct {
+	User_id string `json:"user_id"`
+	Book_id string `json:"book_id"`
+}
+
+type UpdateOrder struct {
+	Id        string  `json:"order_id"`
+	User_id   string  `json:"user_id"`
+	Book_id   string  `json:"book_id"`
+	Payed     float64 `json:"payed"`
+	UpdatedAt string  `json:"updated_at"`
+}
+
+type GetListOrderRequest struct {
+	Limit  int32
+	Offset int32
+}
+
+type GetListOrderResponse struct {
+	Count  int32         `json:"count"`
+	Orders []*OrderGroup `json:"orders"`
+}
