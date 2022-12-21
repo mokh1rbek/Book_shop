7d3068a90e18a5692bc80a3efce55f04b35c3f9e
mokhirbek <mokhirbekabduvaliev@gmail.com>
2022-12-21 18:22:46 -0500

    book shop

---
+++ storage/postgres/order.go
@@ -0,0 +1,241 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4/pgxpool"
+
+	"github.com/mokh1rbek/Book_shop/models"
+	"github.com/mokh1rbek/Book_shop/pkg/helper"
+)
+
+type OrderRepo struct {
+	db *pgxpool.Pool
+}
+
+func NewOrderRepo(db *pgxpool.Pool) *OrderRepo {
+	return &OrderRepo{
+		db: db,
+	}
+}
+
+func (f *OrderRepo) Create(ctx context.Context, order *models.CreateOrder) (string, error) {
+
+	var (
+		id            = uuid.New().String()
+		payed         sql.NullFloat64
+		queryGetPrice string
+		query         string
+	)
+
+	queryGetPrice = `
+		SELECT
+			price
+		FROM
+			books
+		WHERE book_id = $1
+	`
+
+	query = `
+		INSERT INTO orders(
+			order_id,
+			user_id,
+			book_id,
+			payed,
+			updated_at
+		) VALUES ( $1, $2, $3, $4, now() )
+	`
+
+	err := f.db.QueryRow(ctx, queryGetPrice, order.BookId).
+		Scan(
+			&payed,
+		)
+
+	if err != nil {
+		return "", err
+	}
+
+	_, err = f.db.Exec(ctx, query,
+		id,
+		order.UserId,
+		order.BookId,
+		payed,
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+func (f *OrderRepo) GetByPKey(ctx context.Context, pkey *models.OrderPrimarKey) (*models.Order, error) {
+
+	var (
+		id         sql.NullString
+		user_id    sql.NullString
+		book_id    sql.NullString
+		payed      sql.NullFloat64
+		created_at sql.NullString
+	)
+
+	query := `
+		SELECT
+			order_id,
+			user_id,
+			book_id,
+			payed,
+			updated_at
+		FROM
+			orders
+		WHERE order_id = $1
+	`
+
+	err := f.db.QueryRow(ctx, query, pkey.Id).
+		Scan(
+			&id,
+			&user_id,
+			&book_id,
+			&payed,
+			&created_at,
+		)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.Order{
+		Id:        id.String,
+		UserId:    user_id.String,
+		BookId:    book_id.String,
+		Payed:     payed.Float64,
+		CreatedAt: created_at.String,
+	}, nil
+}
+
+func (f *OrderRepo) GetList(ctx context.Context, req *models.GetListOrderRequest) (*models.GetListOrderResponse, error) {
+
+	var (
+		resp   = models.GetListOrderResponse{}
+		offset = " OFFSET 0"
+		limit  = " LIMIT 10"
+	)
+
+	if req.Limit > 0 {
+		limit = fmt.Sprintf(" LIMIT %d", req.Limit)
+	}
+
+	if req.Offset > 0 {
+		offset = fmt.Sprintf(" OFFSET %d", req.Offset)
+	}
+
+	query := `
+		SELECT
+			COUNT(*) OVER(),
+			users.first_name || ' ' || users.last_name as fullname,
+			SUM(orders.payed) as total_payed
+		FROM
+			orders
+		JOIN users ON orders.user_id = users.user_id
+		GROUP BY fullname
+	`
+
+	query += offset + limit
+
+	rows, err := f.db.Query(ctx, query)
+
+	for rows.Next() {
+
+		var (
+			fullname    sql.NullString
+			total_payed sql.NullFloat64
+		)
+
+		err := rows.Scan(
+			&resp.Count,
+			&fullname,
+			&total_payed,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		resp.Orders = append(resp.Orders, &models.OrderGroup{
+			FullName:   fullname.String,
+			TotalPayed: total_payed.Float64,
+		})
+
+	}
+
+	return &resp, err
+}
+
+func (f *OrderRepo) Update(ctx context.Context, req *models.UpdateOrder) (int64, error) {
+
+	var (
+		query         = ""
+		queryGetPrice = ""
+		params        map[string]interface{}
+	)
+
+	queryGetPrice = `
+		SELECT
+			price
+		FROM
+			books
+		WHERE book_id = $1
+	`
+
+	query = `
+		UPDATE
+			orders
+		SET
+			user_id = :user_id,
+			book_id = :book_id,
+			payed = :payed,
+			updated_at = now(),
+		WHERE order_id = :order_id
+	`
+
+	err := f.db.QueryRow(ctx, queryGetPrice, req.BookId).
+		Scan(
+			&req.Payed,
+		)
+
+	fmt.Println(req.Payed)
+	if err != nil {
+		return 0, err
+	}
+
+	req.Payed = 15.2222222
+
+	params = map[string]interface{}{
+		"order_id": req.Id,
+		"user_id":  req.UserId,
+		"book_id":  req.BookId,
+		"payed":    req.Payed,
+	}
+
+	query, args := helper.ReplaceQueryParams(query, params)
+
+	rowsAffected, err := f.db.Exec(ctx, query, args...)
+	if err != nil {
+		return 0, err
+	}
+
+	return rowsAffected.RowsAffected(), nil
+}
+
+func (f *OrderRepo) Delete(ctx context.Context, req *models.OrderPrimarKey) error {
+
+	_, err := f.db.Exec(ctx, "DELETE FROM orders WHERE order_id = $1", req.Id)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
